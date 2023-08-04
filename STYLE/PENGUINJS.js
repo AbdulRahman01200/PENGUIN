014f522e72d51c3a92c7088da7bc9a365a9df748
AbdulRahman01200 <141459722+AbdulRahman01200@users.noreply.github.com>
2023-08-05 01:22:17 +0300

    Add files via upload

---
+++ STYLE/PENGUINJS.js
@@ -0,0 +1,6 @@
+function OUT() {
+    document.getElementById('logo').style.color = 'aliceblue'
+    document.getElementById('logo').style.fontFamily = 'Montserrat'
+    document.getElementById('logo').style.transition = '1s'
+    document.getElementById('logo').style.border = '0px'
+}
